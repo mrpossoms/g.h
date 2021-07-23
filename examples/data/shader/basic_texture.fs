21683ab100a9cd0f11319e10b908eb93e63005b7
Kirk Roerig <kirk.roerig@advanced-space.com>
2021-07-23 08:21:07 -0600

    Missed basic_texture.fs

---
+++ examples/data/shader/basic_texture.fs
@@ -0,0 +1,12 @@
+#version 410
+in vec4 v_screen_pos;
+in vec2 v_uv;
+
+uniform sampler2D u_texture;
+
+out vec4 color;
+
+void main (void)
+{
+	color = texture(u_texture, vec2(1.0 - v_uv.x, v_uv.y));
+}
