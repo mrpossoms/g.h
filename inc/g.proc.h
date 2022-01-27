9e5f3328fc31d3a1e262d36ceabf73770e232fb8
Kirk Roerig <mr.possoms@gmail.com>
2022-01-27 06:34:38 -0700

    Some rework of the thread pool

--- inc/g.proc.h
+++ inc/g.proc.h
@@ -13,31 +13,67 @@ namespace proc
 template<size_t POOL_SIZE>
 struct thread_pool
 {
+	struct task
+	{
+		std::function<void(void)> work = nullptr;
+		std::function<void(void)> on_finish = nullptr;
+	};
+
+	struct worker
+	{
+		bool running;
+		std::thread thread;
+		std::mutex trigger;
+		task current_task;
+	};
 
 	thread_pool()
 	{
 		for (unsigned i = 0; i < POOL_SIZE; i++)
 		{
 			// prevent the worker from looping without a task
-			worker_locks[i].lock();
-			worker_tasks[i] = nullptr;
-			worker_running[i] = true;
+			workers[i].trigger.lock();
+			workers[i].running = true;
 
 			// start the worker thread
-			workers[i] = std::thread([this, i]() {
-				while(worker_running[i])
+			workers[i].thread = std::thread([this, i]() {
+				auto& me = workers[i];
+				while(me.running)
 				{
-					worker_locks[i].lock();
+					me.trigger.lock();
 
 					// execute task
-					if (worker_tasks[i])
+					if (me.current_task.work)
 					{
-						worker_tasks[i]();
-						worker_tasks[i] = nullptr;
+						me.current_task.work();
+						me.current_task.work = nullptr;
 					}
 
-					// reinsert self into idle worker queue
-					idle_workers.push_front(i);
+					// if a dispatch was provided execute here
+					if (me.current_task.on_finish)
+					{
+						std::scoped_lock lock(finish_mutex);
+						pending_finishes.push_front(me.current_task.on_finish);
+					}
+
+					{ // manage new tasks or going dormant
+						std::scoped_lock lock(task_mutex);
+
+						// try to grab a new task
+						if (pending_tasks.size() > 0)
+						{
+							me.current_task = pending_tasks.back();
+							pending_tasks.pop_back();
+
+							me.trigger.unlock();
+						}
+						else
+						{   // no more work to do.
+							// reinsert self into idle worker queue
+							std::scoped_lock lock(idle_worker_mutex);
+							idle_workers.push_front(i);
+						}
+					}
 				}
 			});
 
@@ -48,37 +84,61 @@ struct thread_pool
 
 	~thread_pool()
 	{
+		pending_tasks.clear();
+
 		for (unsigned i = 0; i < POOL_SIZE; i++)
 		{
-			worker_running[i] = false;
-			worker_locks[i].unlock();
+			workers[i].running = false;
+			workers[i].trigger.unlock();
 
-			if (workers[i].joinable()) { workers[i].join(); }
+			if (workers[i].thread.joinable()) { workers[i].thread.join(); }
 		}
 	}
 
-	bool run(std::function<void(void)> task)
+	void run(std::function<void(void)> task, std::function<void(void)> on_finish=nullptr)
 	{
-		if (idle_workers.size() == 0) { return false; }
+		{ // try to start work immediately if there is a free worker
+			std::scoped_lock lock(idle_worker_mutex);
+			if (idle_workers.size() > 0)
+			{
+				auto i = idle_workers.back();
+				idle_workers.pop_back();
 
-		auto i = idle_workers.back();
-		idle_workers.pop_back();
-		worker_tasks[i] = task;
-		worker_locks[i].unlock();
+				workers[i].current_task = { task, on_finish };
+				workers[i].trigger.unlock();
 
-		return true;
+				return;
+			}
+		}
+
+		{ // otherwise, queue the task for the next available
+			std::scoped_lock lock(task_mutex);
+			pending_tasks.push_front(thread_pool::task{task, on_finish});
+		}
 	}
 
+	void update()
+	{
+		std::scoped_lock lock(finish_mutex);
+		while (pending_finishes.size() > 0)
+		{
+			pending_finishes.back()();
+			pending_finishes.pop_back();
+		}
+	}
 
 	size_t idle_threads() { return idle_workers.size(); }
 private:
-	std::thread workers[POOL_SIZE];
-	std::function<void(void)> worker_tasks[POOL_SIZE];
-	std::mutex worker_locks[POOL_SIZE];
-	bool worker_running[POOL_SIZE];
+	thread_pool::worker workers[POOL_SIZE];
 
+	std::mutex idle_worker_mutex;
 	std::deque<unsigned> idle_workers;
-	std::deque<std::function<void(void)>> tasks;
+
+	std::mutex task_mutex;
+	std::deque<task> pending_tasks;
+
+	std::mutex finish_mutex;
+	std::deque<std::function<void(void)>> pending_finishes;
 };
 
 }; // namespace proc
--- tests/thread-pool.cpp
+++ tests/thread-pool.cpp
@@ -7,9 +7,9 @@
  */
 TEST
 {
-    g::proc::thread_pool<3> pool;
+    g::proc::thread_pool<2> pool;
 
-    assert(pool.idle_threads() == 3);
+    assert(pool.idle_threads() == 2);
 
     int res[3] = {};
     int a[3] = { 1, 2, 3 };
@@ -18,14 +18,21 @@ TEST
     for (unsigned i = 0; i < 3; i++)
     {
         pool.run([&res, &a, &b, i](){
-            std::this_thread::sleep_for(std::chrono::seconds(1));
             res[i] = a[i] + b[i];
+        },
+        [i]() {
+            std::cerr << "worker " << i << " finished!" << std::endl;
         });
     }
 
     assert(pool.idle_threads() == 0);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    assert(pool.idle_threads() == 3);
+    assert(pool.idle_threads() == 2);
+
+    for (int i = 0; i < 3; i++)
+    {
+        pool.update();
+    }
 
     for (unsigned i = 0; i < 3; i++)
     {
