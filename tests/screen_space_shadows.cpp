836ea5a7abe5c3ec6172deeb46587a8925d02198
Kirk Roerig <mr.possoms@gmail.com>
2022-06-11 08:03:11 -0600

    Basic sanity check works!

--- tests/screen_space_shadows.cpp
+++ tests/screen_space_shadows.cpp
@@ -5,14 +5,17 @@ using namespace xmath;
 
 vec<3> world_to_screen(const mat<4,4>& transform, const vec<3>& p)
 {
-    p_proj = transform * p;
+    std::cerr << __func__ << std::endl;
+    auto p_proj = transform * vec<4>{ p[0], p[1], p[2], 1 };
+    std::cerr << p_proj.to_string() << std::endl;
     return (p_proj / p_proj[3]).slice<3>();
 }
 
 vec<3> screen_to_world(const mat<4,4>& proj, const mat<4,4>& view, const vec<3>& p)
 {
-    auto z = -1.0 / ((p[2] * proj[3][2]) - proj[2][2]);
-    auto w = z * proj[3][2];
+    std::cerr << __func__ << std::endl;
+    float z = -1.f / ((p[2] * proj[3][2]) - proj[2][2]);
+    float w = z * proj[3][2];
     auto p_screen = vec<4>{p[0] * w, p[1] * w, z, w};
 
     auto p_view = proj.invert() * p_screen;
@@ -21,7 +24,7 @@ vec<3> screen_to_world(const mat<4,4>& proj, const mat<4,4>& view, const vec<3>&
     // "   float z = -1.0 / ((p[2] * P[3][2]) - P[2][2]);"
     // "   float w = z * P[3][2];"
     // "   vec4 p_w = vec4(p.xy * w, z, w);"
-    return p_world;
+    return p_world.slice<3>();
 }
 
 /**
@@ -32,11 +35,12 @@ TEST
 {
     { // ensure that a point can be retrieved after projection
         auto P = mat<4,4>::perspective(1, 100, M_PI / 2, 1.f);
+        auto V = mat<4, 4>::I();
 
         auto p = vec<3>{0, 0, -10};
         auto p_prime = world_to_screen(P, p);
 
-        auto p0 = screen_to_world(P, p_prime);
+        auto p0 = screen_to_world(P, V, p_prime);
 
         assert(p0.is_near(p, 0.000001));
     }
