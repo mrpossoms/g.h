9d4cda6cd15f96f0f273728a81c906d523986297
Kirk Roerig <mr.possoms@gmail.com>
2022-06-26 09:59:30 -0600

    Got it I think

--- tests/screen_space_shadows.cpp
+++ tests/screen_space_shadows.cpp
@@ -22,12 +22,19 @@ vec<3> screen_to_world(const mat<4,4>& proj, const mat<4,4>& view, const vec<3>&
     std::cerr << __func__ << std::endl;
     std::cerr << "persp div " << p.to_string() << std::endl;
 
-    float z = -1.f / ((p[2] * proj[3][2]) - proj[2][2]);
-    float w = z * proj[3][2];
-    auto p_screen = vec<4>{p[0] * w, p[1] * w, z, w};
-    std::cerr << "screen " << p_screen.to_string() << std::endl;
+    auto a = proj[3][2];
+    auto b = proj[2][2];
 
-    auto p_view = proj.invert() * p_screen;
+    auto _z = -1 / ((p[2] * a) - b);
+    // undo the perspective divide and projection
+    auto p_view = xmath::vec<4>{
+        p[0] * (_z * a),
+        p[1] * (_z * a),
+        _z,
+        1//{_z * a}
+    };
+    std::cerr << "screen " << p.to_string() << std::endl;
+
     std::cerr << "view " << p_view.to_string() << std::endl;
     auto p_world = view.transpose().invert() * p_view;
     std::cerr << "world " << p_view.to_string() << std::endl;
@@ -46,7 +53,11 @@ bool is_in_shadow(
     const mat<4,4>& light_view,
     const mat<4,4>& light_proj)
 {
-    return false;
+
+    auto cam_w = screen_to_world(cam_proj, cam_view, cam_p);
+    auto light_w = screen_to_world(light_proj, light_view, light_p);
+
+    return (cam_w - light_w).magnitude() > 0.001f;
 }
 
 /**
@@ -80,14 +91,30 @@ TEST
         auto p_light_screen = world_to_screen(P, V, p_world);
         auto p_cam_screen = world_to_screen(P, mat<4,4>::I(), p_world);
 
-        // assert(is_in_shadow(
-        //     p_cam_screen,
-        //     p_light_screen,
-        //     mat<4,4>::I(), P,
-        //     V, P
-        // ) == false);
+         assert(is_in_shadow(
+             p_cam_screen,
+             p_light_screen,
+             mat<4,4>::I(), P,
+             V, P
+         ) == false);
     }
 
+    { // simple case with light behind viewer, and the viewer occludes
+
+        auto P = mat<4,4>::perspective(1, 100, M_PI / 2, 1.f);
+        auto V = mat<4, 4>::look_at({0, 0, 10}, {0, 0, -1}, {0, 1, 0});
+
+        auto p_world = vec<3>{0, 0, -10};
+        auto p_light_screen = world_to_screen(P, V, { 0, 0, 0 });
+        auto p_cam_screen = world_to_screen(P, mat<4,4>::I(), p_world);
+
+         assert(is_in_shadow(
+             p_cam_screen,
+             p_light_screen,
+             mat<4,4>::I(), P,
+             V, P
+         ) == true);
+    }
 
 	return 0;
 }
